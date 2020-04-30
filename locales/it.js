a00b8216b0b39df9b5ac68e1742cdc5569f6048a
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-04-29 20:08:33 -0400

    start translation

--- locales/it.js
+++ locales/it.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'main-title': 'Ciao',
+	'main-title-before-man': 'Battagliando COVID-19, un',
+	'main-title-after-man': 'per volta',
+	'the-community-platform-for-exchanging-needed-materials-using-requests-and-fulfillments': 'The community platform for exchanging needed materials using requests and fulfillments.',
 }
